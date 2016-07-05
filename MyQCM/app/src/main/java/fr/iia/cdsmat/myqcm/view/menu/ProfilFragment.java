48c8dba3390b1e2dd0a88883da4815225253dbbc
antoinetrouve <antoinetrouve.france@gmail.com>
2016-07-05 22:35:15 +0200

    Creation menu

--- MyQCM/app/src/main/java/fr/iia/cdsmat/myqcm/view/menu/ProfilFragment.java
+++ MyQCM/app/src/main/java/fr/iia/cdsmat/myqcm/view/menu/ProfilFragment.java
@@ -2,14 +2,22 @@ package fr.iia.cdsmat.myqcm.view.menu;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 import fr.iia.cdsmat.myqcm.R;
 import fr.iia.cdsmat.myqcm.configuration.MyQCMConstants;
+import fr.iia.cdsmat.myqcm.data.UserSQLiteAdapter;
 import fr.iia.cdsmat.myqcm.data.webservice.UserWSAdapter;
+import fr.iia.cdsmat.myqcm.entity.User;
 
 /**
  * Class managing Profil's fragment depending of Menu Activity
@@ -17,6 +25,7 @@ import fr.iia.cdsmat.myqcm.data.webservice.UserWSAdapter;
  * @version 1.0 - 04/04/2016
  */
 public class ProfilFragment extends Fragment {
+    UserSQLiteAdapter userSQLiteAdapter;
 
     public ProfilFragment() {
     }
@@ -25,8 +34,47 @@ public class ProfilFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
+        //Inflate the fragment layout file
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_profil, container, false);
+
+        FloatingActionButton fab = (FloatingActionButton) getActivity().findViewById(R.id.fab);
+        fab.setVisibility(fab.INVISIBLE);
+
+        userSQLiteAdapter = new UserSQLiteAdapter(getActivity().getBaseContext());
+        userSQLiteAdapter.open();
+        ArrayList<User> users = new ArrayList<>();
+        users = userSQLiteAdapter.getAllUser();
+        if (users != null) {
+            User userDB = users.get(0);
+            System.out.println("Userid = " + userDB.getUsername());
+            String username = userDB.getUsername();
+            RelativeLayout relativeLayoutProfilHeader = (RelativeLayout) rootView.findViewById(R.id.profile_layout_header);
+
+            //Header
+            RelativeLayout relativeLayoutProfil = (RelativeLayout) relativeLayoutProfilHeader.findViewById(R.id.profile_layout);
+            TextView textViewProfilName = (TextView) relativeLayoutProfil.findViewById(R.id.user_profile_name);
+            textViewProfilName.setText(username);
+
+            //Content profil
+            LinearLayout linearLayoutContent = (LinearLayout) relativeLayoutProfilHeader.findViewById(R.id.profile_layout_content);
+            TextView textViewProfilUsername = (TextView) linearLayoutContent.findViewById(R.id.user_profil_identifiant);
+            textViewProfilUsername.setText(MyQCMConstants.CONST_PROFIL_USERNAME + " " + username);
+
+            TextView textViewProfilEmail = (TextView) linearLayoutContent.findViewById(R.id.user_profil_email);
+            textViewProfilEmail.setText(MyQCMConstants.CONST_PROFIL_EMAIL + " " + userDB.getEmail());
+
+
+
+
+
+
+
+        }
+
+
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profil, container, false);
+        return rootView;
 
 
 
--- MyQCM/app/src/main/java/fr/iia/cdsmat/myqcm/view/menu/RulesFragment.java
+++ MyQCM/app/src/main/java/fr/iia/cdsmat/myqcm/view/menu/RulesFragment.java
@@ -2,12 +2,17 @@ package fr.iia.cdsmat.myqcm.view.menu;
 
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import fr.iia.cdsmat.myqcm.R;
+import fr.iia.cdsmat.myqcm.configuration.MyQCMConstants;
 
 /**
  * Class managing Rules's fragment depending of Menu Activity
@@ -25,8 +30,11 @@ public class RulesFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_rules, container, false);
+        //Inflate the fragment layout file
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_rules, container, false);
+        FloatingActionButton fab = (FloatingActionButton) getActivity().findViewById(R.id.fab);
+        fab.setVisibility(fab.INVISIBLE);
+        return rootView;
     }
 
 }
